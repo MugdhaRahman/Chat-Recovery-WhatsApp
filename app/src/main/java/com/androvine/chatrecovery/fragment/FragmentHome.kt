42dc42a70fed8b33c7a4cc20457e6be2eb4797b3
Mugdha <mugdharahman11@gmail.com>
2023-10-14 19:28:21 +0600

    bottom nav added

--- app/src/main/java/com/androvine/chatrecovery/fragment/FragmentHome.kt
+++ app/src/main/java/com/androvine/chatrecovery/fragment/FragmentHome.kt
@@ -1,21 +1,26 @@
 package com.androvine.chatrecovery.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.androvine.chatrecovery.R
+import androidx.fragment.app.Fragment
+import com.androvine.chatrecovery.databinding.FragmentHomeBinding
 
 class FragmentHome : Fragment() {
 
+    private val binding: FragmentHomeBinding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+
+
+
+        return binding.root
     }
 
 }
