0346e53fec13f61e6441ae453b3f8c2729ce08c8
Mugdha <mugdharahman11@gmail.com>
2023-10-24 23:24:05 +0600

    call item layout

---
+++ app/src/main/java/com/androvine/chatrecovery/adapter/CallAdapter.kt
@@ -0,0 +1,42 @@
+package com.androvine.chatrecovery.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.androvine.chatrecovery.databinding.ItemCallsBinding
+import com.androvine.chatrecovery.models.CallModel
+
+class CallAdapter(private val context: Context, private val callList: MutableList<CallModel>) :
+    RecyclerView.Adapter<CallAdapter.CallViewHolder>() {
+    override fun onCreateViewHolder(
+        viewGroup: ViewGroup,
+        viewType: Int
+    ): CallAdapter.CallViewHolder {
+        val binding = ItemCallsBinding.inflate(
+            LayoutInflater.from(viewGroup.context),
+            viewGroup,
+            false
+        )
+        return CallViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CallViewHolder, position: Int) {
+
+        val callModel = callList[position]
+        holder.binding.tvName.text = callModel.user
+
+        holder.binding.tvCallTime.text = callModel.time.toString()
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return callList.size
+    }
+
+
+    class CallViewHolder(val binding: ItemCallsBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+}
