c3973c44e5a96b919eb94fbfe5ad223547758d3c
Mugdha <mugdharahman11@gmail.com>
2023-10-26 23:59:42 +0600

    call list

---
+++ app/src/main/java/com/androvine/chatrecovery/adapter/UserAdapter.kt
@@ -0,0 +1,55 @@
+package com.androvine.chatrecovery.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.androvine.chatrecovery.databinding.ItemUserBinding
+import com.androvine.chatrecovery.models.MessageModel
+import java.sql.Date
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class UserAdapter(private val userList: MutableList<MessageModel>) :
+    RecyclerView.Adapter<UserAdapter.MessageViewHolder>() {
+    override fun onCreateViewHolder(
+        viewGroup: ViewGroup,
+        viewType: Int
+    ): MessageViewHolder {
+        val binding = ItemUserBinding.inflate(
+            LayoutInflater.from(viewGroup.context),
+            viewGroup,
+            false
+        )
+        return MessageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+
+
+        val messageModel = userList[position]
+
+        holder.binding.tvName.text = messageModel.user
+
+        val timestamp = messageModel.time
+        val dateFormat = SimpleDateFormat("h:mm a", Locale.getDefault())
+        val formattedTime = dateFormat.format(Date(timestamp))
+        holder.binding.tvTime.text = formattedTime
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    fun updateList(messageList1: List<MessageModel>) {
+        this.userList.clear()
+        this.userList.addAll(messageList1)
+        notifyDataSetChanged()
+    }
+
+
+    class MessageViewHolder(val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+}
--- app/src/main/java/com/androvine/chatrecovery/fragment/FragmentHome.kt
+++ app/src/main/java/com/androvine/chatrecovery/fragment/FragmentHome.kt
@@ -1,11 +1,19 @@
 package com.androvine.chatrecovery.fragment
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.androvine.chatrecovery.adapter.UserAdapter
 import com.androvine.chatrecovery.databinding.FragmentHomeBinding
+import com.androvine.chatrecovery.db.MessageDBHelper
+import com.androvine.chatrecovery.models.MessageModel
 
 class FragmentHome : Fragment() {
 
@@ -13,14 +21,47 @@ class FragmentHome : Fragment() {
         FragmentHomeBinding.inflate(layoutInflater)
     }
 
+    private val userList = mutableListOf<MessageModel>()
+    private lateinit var userAdapter: UserAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
+        val intentFilter = IntentFilter("new_item_message")
+        requireActivity().registerReceiver(broadcastReceiver, intentFilter)
 
-
+        userAdapter = UserAdapter(mutableListOf())
+        binding.rvChatList.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = userAdapter
+        }
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadCallListData()
+    }
+
+
+    private fun loadCallListData() {
+        val dbHelper = MessageDBHelper(requireActivity())
+    }
+
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "new_item_call") {
+                loadCallListData()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        requireActivity().unregisterReceiver(broadcastReceiver)
     }
 
 }
