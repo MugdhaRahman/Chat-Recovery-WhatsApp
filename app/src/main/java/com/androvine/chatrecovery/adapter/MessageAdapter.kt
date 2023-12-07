eac9cc6f2494fd4894a4fdb9d408598756702962
Mugdha <mugdharahman11@gmail.com>
2023-12-08 01:46:48 +0600

    delete item done

--- app/src/main/java/com/androvine/chatrecovery/adapter/MessageAdapter.kt
+++ app/src/main/java/com/androvine/chatrecovery/adapter/MessageAdapter.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.androvine.chatrecovery.databinding.BottomsheetCallListBinding
 import com.androvine.chatrecovery.databinding.ItemMessageBinding
+import com.androvine.chatrecovery.db.MessageDBHelper
 import com.androvine.chatrecovery.models.MessageModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import java.sql.Date
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -58,11 +61,71 @@ class MessageAdapter(
             holder.binding.msgDate.visibility = View.GONE
         }
 
+        holder.binding.cvMsg.setOnLongClickListener {
+            val dialog = BottomSheetDialog(it.context)
+            val binding = BottomsheetCallListBinding.inflate(
+                LayoutInflater.from(it.context),
+                null,
+                false
+            )
+            dialog.setContentView(binding.root)
+            dialog.setCancelable(true)
+
+            binding.btnDelete.setOnClickListener {
+
+                deleteSingleMessage(position)
+                dialog.dismiss()
+
+//                //show delete dialog
+//
+//                val dialog = Dialog(it.context)
+//                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+//                dialog.setCancelable(true)
+//                dialog.setContentView(R.layout.dialog_delete_files)
+//                dialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+//                dialog.window!!.setLayout(
+//                    ViewGroup.LayoutParams.MATCH_PARENT,
+//                    ViewGroup.LayoutParams.WRAP_CONTENT
+//                )
+//
+//                dialog.findViewById<androidx.appcompat.widget.AppCompatTextView>(R.id.cancel)
+//                    .setOnClickListener {
+//                        dialog.dismiss()
+//                    }
+//                dialog.findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.delete)
+//                    .setOnClickListener {
+//                        //delete call item
+//
+//
+//                        dialog.dismiss()
+//                    }
+//                dialog.show()
+//
+//                dialog.dismiss()
+//
+            }
+
+            dialog.show()
+            true
+        }
+
 
     }
 
     override fun getItemCount(): Int {
         return userList.size
+    }
+
+    fun deleteSingleMessage(position: Int) {
+        val deletedMessage = userList[position]
+        val dbHelper = MessageDBHelper(context)
+        val deletedRows = dbHelper.deleteMessageItem(deletedMessage.id)
+
+        if (deletedRows > 0) {
+            userList.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, itemCount)
+        }
     }
 
     //update userList
--- app/src/main/java/com/androvine/chatrecovery/adapter/UserAdapter.kt
+++ app/src/main/java/com/androvine/chatrecovery/adapter/UserAdapter.kt
@@ -1,13 +1,17 @@
 package com.androvine.chatrecovery.adapter
 
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.startActivity
+import android.view.Window
 import androidx.recyclerview.widget.RecyclerView
+import com.androvine.chatrecovery.R
 import com.androvine.chatrecovery.activity.MessageViewActivity
 import com.androvine.chatrecovery.databinding.ItemUserBinding
+import com.androvine.chatrecovery.db.MessageDBHelper
 import com.androvine.chatrecovery.models.MessageModel
 import java.sql.Date
 import java.text.SimpleDateFormat
@@ -49,10 +53,62 @@ class UserAdapter(private val context: Context, private val userList: MutableLis
         holder.binding.tvTime.text = formattedTime
 
 
+        holder.binding.messageItem.setOnLongClickListener {
+
+            holder.binding.tvTime.visibility = View.GONE
+            holder.binding.ivDeleteUser.visibility = View.VISIBLE
+
+            true
+        }
+
+        holder.binding.ivDeleteUser.setOnClickListener {
+
+            val dialog = Dialog(it.context)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.dialog_delete_files)
+            dialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
+            dialog.window!!.setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            dialog.findViewById<androidx.appcompat.widget.AppCompatTextView>(R.id.cancel)
+                .setOnClickListener {
+                    holder.binding.tvTime.visibility = View.VISIBLE
+                    holder.binding.ivDeleteUser.visibility = View.GONE
+                    dialog.dismiss()
+                }
+            dialog.findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.delete)
+                .setOnClickListener {
+
+                    deleteUser(position)
+
+                    holder.binding.tvTime.visibility = View.VISIBLE
+                    holder.binding.ivDeleteUser.visibility = View.GONE
+
+                    dialog.dismiss()
+                }
+            dialog.show()
+
+        }
+
     }
 
     override fun getItemCount(): Int {
         return userList.size
+    }
+
+    fun deleteUser(position: Int) {
+        val deletedUser = userList[position]
+        val dbHelper = MessageDBHelper(context)
+        val deletedRows = dbHelper.deleteByUser(deletedUser.user)
+
+        if (deletedRows > 0) {
+            userList.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, itemCount)
+        }
     }
 
     //update userList
