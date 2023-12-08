18b2b0b961b415a3b76a51cb10f4da3db15611e2
Mugdha <mugdharahman11@gmail.com>
2023-12-09 01:48:34 +0600

    dark mode off

--- app/src/main/java/com/androvine/chatrecovery/activity/SettingsActivity.kt
+++ app/src/main/java/com/androvine/chatrecovery/activity/SettingsActivity.kt
@@ -88,25 +88,25 @@ class SettingsActivity : AppCompatActivity() {
             }
 
 
-            val darkThemePref =
-                findPreference<androidx.preference.ListPreference>("pref_dark_theme")
-            darkThemePref?.setOnPreferenceChangeListener { _, newValue ->
-                when (newValue as String) {
-                    "light_theme" -> {
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    }
-
-                    "dark_theme" -> {
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                    }
-
-                    else -> {
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-                    }
-                }
-                requireActivity().recreate()
-                true
-            }
+//            val darkThemePref =
+//                findPreference<androidx.preference.ListPreference>("pref_dark_theme")
+//            darkThemePref?.setOnPreferenceChangeListener { _, newValue ->
+//                when (newValue as String) {
+//                    "light_theme" -> {
+//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//                    }
+//
+//                    "dark_theme" -> {
+//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//                    }
+//
+//                    else -> {
+//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+//                    }
+//                }
+//                requireActivity().recreate()
+//                true
+//            }
 
         }
 
